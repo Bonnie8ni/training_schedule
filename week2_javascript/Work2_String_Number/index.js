1b9a8c4ad33d9bda30e06648d94892a5b3dff344
BonnieYu <Bonnie_Yu@wistron.com>
2018-03-06 11:36:35 +0800

    String & Number - 1

---
+++ week2_javascript/Work2_String_Number/index.js
@@ -0,0 +1,65 @@
+//1. 一個陣列中有許多個字串，寫一個function找出這些字串最長的共同字首。
+var longestCommonWord = function (strs) {
+
+    strs = strs.split(',');
+    if (strs == null || strs.length == 0) return "";
+    var same = strs[0];
+
+    for (var i = strs.length - 1; i >= 0; i--) {
+
+        var str = strs[i];
+
+        for (var j = same.length - 1; j >= 0; j--) {
+
+            if (same[j] != str.charAt(j)) {
+                break;
+            }
+        }
+        same = same.replace(same[j], "$");
+    }
+
+    console.log("answer2:" + same);
+    same = same.replace(/\$/g, "");
+    return same;
+}
+
+console.log(longestCommonWord('abc,cac,ebc'));
+
+
+
+
+//2. 將一個字串反轉後回傳。
+var reverseStr = function (strs) {
+    var reverse = [];
+
+    for (var i = strs.length - 1; i >= 0; i--) {
+        reverse.push(strs[i]);
+    }
+
+    return reverse.join('');
+}
+//console.log(reverseStr('hello'));
+
+
+
+
+//3. 給兩個字串s與t，回傳t是否為s的重組字
+var compareStr = function (first_str, second_str) {
+
+    if (first_str.sort().join('') !== second_str.sort().join('')) return false;
+    else return true;
+}
+//console.log(compareStr('abc,cba'));
+
+
+
+
+//4. 給一個英文字串，將裡面的母音字母反轉。
+
+//5. 給二進制字串，將其換算成對應的十進制數字，需自己寫function
+
+//6. 將給定數字轉換成二進制字串。如果字串長度不足 8 位，則在前面補 0 到滿8位。
+
+//7. 將一個數字每個位數相加，直到剩個位數為止。
+
+//8. 反轉一個int整數。
