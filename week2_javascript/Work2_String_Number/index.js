6ca91c8e6edd00ce77737f7586098d5bc3e0e6d8
BonnieYu <Bonnie_Yu@wistron.com>
2018-03-08 17:12:43 +0800

    Array - Update compare[indexj] !== allStrs[indexi][indexj]

--- week2_javascript/Work2_String_Number/index.js
+++ week2_javascript/Work2_String_Number/index.js
@@ -1,19 +1,20 @@
 
 // 1.一個陣列中有許多個字串，寫一個function找出這些字串最長的共同字首。(Bonnie修改題目為從字串後面找)
 const longestCommonWord = ((strs) => {
+  if (!strs) return '';
   const allStrs = Array.from(strs).reverse().join('').split(',');
   if (allStrs.length < 2) return '';
   let compare = Array.from(allStrs[0]);
-  allStrs.forEach((value, indexi) => {
-    compare.forEach((item, indexj) => {
-      if (compare[indexj] !== allStrs[indexi][indexj]) {
-        compare = compare.slice(0, indexj);
+  allStrs.forEach((value) => {
+    compare.forEach((item, index) => {
+      if (item !== value[index]) {
+        compare = compare.slice(0, index);
       }
     });
   });
   return compare.reverse().join('');
 });
-console.log(longestCommonWord('cebdaf,cebaf,cedfdfdaf'));
+console.log(longestCommonWord('aeeasdfsaewaf,esadvdsfaf,asdfwaeeaf'));
 
 
 // 2.將一個字串反轉後回傳。
@@ -22,15 +23,16 @@ const reverseStr = ((strs) => {
   const reverse = strs.split('').reverse().join('');
   return reverse;
 });
-console.log(reverseStr('ABCD'));
+console.log(reverseStr());
 
 
 // 3.給兩個字串s與t，回傳t是否為s的重組字
 const compareStr = ((firstStr, secondStr) => {
-  if (firstStr.length !== secondStr.length) return false;
+  if (!firstStr || !secondStr) return '';
+  if (firstStr.length !== secondStr.length) return '';
   return firstStr.split('').sort().join('') === secondStr.split('').sort().join('');
 });
-console.log(compareStr('ABCD', 'DCAB'));
+console.log(compareStr());
 
 
 // 4.給一個英文字串，將裡面的母音字母反轉。
@@ -48,12 +50,13 @@ const reverseAEIOU = ((strs) => {
   }
   return strsArry.join('');
 });
-console.log(reverseAEIOU('aELLiOau'));
+console.log(reverseAEIOU());
 
 
 // 5.給二進制字串，將其換算成對應的十進制數字，需自己寫function
 // 反轉後再開始
 const binaryToDecimal = ((total) => {
+  if (!total) return '';
   let decimaltotal = 0;
   const binaryStrs = Array.from(total).reverse();
   binaryStrs.forEach((value, index) => {
@@ -63,11 +66,12 @@ const binaryToDecimal = ((total) => {
   });
   return decimaltotal;
 });
-console.log(binaryToDecimal('11000'));
+console.log(binaryToDecimal());
 
 
 // 6. 將給定數字轉換成二進制字串。如果字串長度不足 8 位，則在前面補 0 到滿8位。
 const fillZero = ((strs) => {
+  if (!strs) return '';
   const binaryStr = Array.from(strs.toString(2));
   let i = 0;
   while (i < 8 - binaryStr.length) {
@@ -76,11 +80,12 @@ const fillZero = ((strs) => {
   }
   return binaryStr.join('');
 });
-console.log(fillZero(25));
+console.log(fillZero());
 
 
 // 7.將一個數字每個位數相加，直到剩個位數為止。
 const numberAddition = ((total) => {
+  if (!total) return '';
   let strArray = Array.from(total.toString());
   if (strArray.length === 1) return total;
   let result = 0;
@@ -90,13 +95,14 @@ const numberAddition = ((total) => {
   }
   return result;
 });
-console.log(numberAddition(1234567890));
+console.log(numberAddition());
 
 
 // 8. 反轉一個int整數。
 const reverseInt = ((total) => {
+  if (!total) return '';
   if (total.length === 1) return total;
   const reverse = Array.from(Math.abs(total).toString()).reverse().join('');
-  return (Math.sign(total) < 0) ? reverse * (-1) : reverse;
+  return (Math.sign(total) < 0) ? +reverse * (-1) : +reverse;
 });
-console.log(reverseInt(1234567890));
+console.log(reverseInt());
