4118985720f6deae78163f6c69086ecb4e696e08
BonnieYu <Bonnie_Yu@wistron.com>
2018-03-08 15:30:23 +0800

    Array - Change 深層拷貝

--- week2_javascript/Work3_Array/index.js
+++ week2_javascript/Work3_Array/index.js
@@ -1,6 +1,6 @@
 // 輸入陣列，輸出一個深層複製的陣列。兩者記憶體位置不能一樣。
 const copyArray = ((value) => {
-  const newArray = Array.from(Object.assign(value));
+  const newArray = Array.from(Object.create(value));
   newArray.push('4');
   return newArray;
 });
