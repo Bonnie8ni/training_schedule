c12b83d7db175e0aabcc43916cc7091542c5925e
bonnieyu53 <bonnieyu53@gmail.com>
2018-03-23 22:08:27 +0800

    補齊Menu的html

--- week3_Integration/src/js/menu/menuData.js
+++ week3_Integration/src/js/menu/menuData.js
@@ -43,6 +43,111 @@ const menuData = [
         subMenuUrl: '#',
       },
     ],
+  }, {
+    menuId: uuidv4(),
+    menuName: 'Machine Management',
+    menuIcon: 'fas fa-map-marker-alt',
+    subMenu: [
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+    ],
+  }, {
+    menuId: uuidv4(),
+    menuName: 'Merchandise Management',
+    menuIcon: 'far fa-file-alt',
+    subMenu: [
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+    ],
+  }, {
+    menuId: uuidv4(),
+    menuName: 'Advertising Management',
+    menuIcon: 'far fa-comments',
+    subMenu: [
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+    ],
+  }, {
+    menuId: uuidv4(),
+    menuName: 'Market Analysis',
+    menuIcon: 'fas fa-chart-pie',
+    subMenu: [
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+    ],
+  }, {
+    menuId: uuidv4(),
+    menuName: 'System Setting',
+    menuIcon: 'fas fa-sliders-h',
+    subMenu: [
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+      {
+        subMenuId: uuidv4(),
+        subMenuName: 'Model List',
+        subMenuUrl: '#',
+      },
+    ],
   },
 ];
 
