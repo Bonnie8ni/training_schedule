ade5fcb111e90db2d44b65f6b931a8f7eb539042
BonnieYu <Bonnie_Yu@wistron.com>
2018-03-30 17:22:08 +0800

    Perpage 整理+註解

--- week3_Integration/src/js/features/Perpage.js
+++ week3_Integration/src/js/features/Perpage.js
@@ -9,10 +9,18 @@ export default class Perpage {
     const $tpPage = $($('#tp-page').html());
     const $pageItem = $tpPage.find('.page-item');
     const $pageLink = $pageItem.find('.th-page-link');
+
+    // 頁數於位置+1
     perDefult.name = pageIndex + 1;
+    // 寫入畫面
+    $pageLink.text(perDefult.name);
+
+    // 當在最後一頁時，若將資料刪光，會跳至所在頁前一頁
     if (PageStorage.totalPage() < PageStorage.currentPage) {
       PageStorage.currentPage -= 1;
     }
+
+    // 現在所在頁面若等於點擊頁面則狀態改變
     if (PageStorage.currentPage === perDefult.name) {
       perDefult.active = true;
     } else {
@@ -21,7 +29,7 @@ export default class Perpage {
     if (perDefult.active) {
       $pageItem.addClass('active');
     }
-    $pageLink.text(pageIndex + 1);
+
     // 頁數點擊時
     $pageItem.click((e) => {
       e.preventDefault();
