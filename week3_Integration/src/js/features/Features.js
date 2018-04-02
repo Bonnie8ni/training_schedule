b5bb64c81207f191df94df358fbef49513db371f
bonnieyu53 <bonnieyu53@gmail.com>
2018-04-02 21:29:28 +0800

    把儲存值都轉移去pagination

--- week3_Integration/src/js/features/Features.js
+++ week3_Integration/src/js/features/Features.js
@@ -1,12 +1,5 @@
 import MachineData from '../../api/MachineData';
 
-const SEARCHKEYWORK = {
-  inputKeyword: '',
-  inputAdvanceKeyword: '',
-  selectedValue: '',
-  temporaryData: MachineData,
-};
-
 export default class Features {
   constructor(PageStorage) {
     this.pageStorage = PageStorage;
@@ -21,7 +14,7 @@ export default class Features {
     const $btnSearch = $search.find('.btn-search');
     const $advancedSearch = $search.find('.advanced-search');
     this.$inputKeyword = $search.find('.input-keyword');
-    this.$inputKeyword.val(SEARCHKEYWORK.inputKeyword);
+    this.$inputKeyword.val(this.pageStorage.inputKeyword);
     const $advancedSearchBox = $search.find('.advanced-search-box');
     this.$inputAdvanceKeyword = $search.find('.input-advanced-keyword');
     this.$selectSearch = $search.find('.select-search');
@@ -46,7 +39,7 @@ export default class Features {
     // 進階搜尋
     $advancedSearch.click(() => {
       $advancedSearchBox.show();
-      this.$inputAdvanceKeyword.val(SEARCHKEYWORK.inputKeyword);
+      this.$inputAdvanceKeyword.val(this.pageStorage.inputKeyword);
     });
 
     this.$selectSearch.change(() => {
@@ -109,21 +102,21 @@ export default class Features {
 
     // 確定後將資料新增
     let newDataCombination = [];
-    if (SEARCHKEYWORK.inputKeyword !== '') {
+    if (pageStorage.inputKeyword !== '') {
       pageStorage.machineData.push(machine);
-      SEARCHKEYWORK.temporaryData = [...SEARCHKEYWORK.temporaryData, machine];
-      newDataCombination = pageStorage.machineData.filter(data => data.address.search(SEARCHKEYWORK.inputKeyword) !== -1 || data.region.search(SEARCHKEYWORK.inputKeyword) !== -1);
-    } else if (SEARCHKEYWORK.inputAdvanceKeyword !== '' || SEARCHKEYWORK.selectedValue !== '') {
+      pageStorage.temporaryData = [...pageStorage.temporaryData, machine];
+      newDataCombination = pageStorage.machineData.filter(data => data.address.search(pageStorage.inputKeyword) !== -1 || data.region.search(pageStorage.inputKeyword) !== -1);
+    } else if (pageStorage.inputAdvanceKeyword !== '' || pageStorage.selectedValue !== '') {
       pageStorage.machineData.push(machine);
-      SEARCHKEYWORK.temporaryData = [...SEARCHKEYWORK.temporaryData, machine];
-      if (SEARCHKEYWORK.inputAdvanceKeyword !== '') {
-        newDataCombination = pageStorage.machineData.filter(data => data.status.toString() === SEARCHKEYWORK.selectedValue || (data.address.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1 || data.region.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1));
+      pageStorage.temporaryData = [...pageStorage.temporaryData, machine];
+      if (pageStorage.inputAdvanceKeyword !== '') {
+        newDataCombination = pageStorage.machineData.filter(data => data.status.toString() === pageStorage.selectedValue || (data.address.search(pageStorage.inputAdvanceKeyword) !== -1 || data.region.search(pageStorage.inputAdvanceKeyword) !== -1));
       } else {
-        newDataCombination = pageStorage.machineData.filter(data => data.status.toString() === SEARCHKEYWORK.selectedValue && (data.address.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1 || data.region.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1));
+        newDataCombination = pageStorage.machineData.filter(data => data.status.toString() === pageStorage.selectedValue && (data.address.search(pageStorage.inputAdvanceKeyword) !== -1 || data.region.search(pageStorage.inputAdvanceKeyword) !== -1));
       }
     } else {
       pageStorage.machineData.push(machine);
-      SEARCHKEYWORK.temporaryData = [...SEARCHKEYWORK.temporaryData, machine];
+      pageStorage.temporaryData = [...pageStorage.temporaryData, machine];
       newDataCombination = [...pageStorage.machineData];
     }
     pageStorage.machineData = newDataCombination;
@@ -138,11 +131,11 @@ export default class Features {
   // 搜尋資料
   searchData() {
     const { pageStorage, $inputKeyword } = this;
-    SEARCHKEYWORK.inputKeyword = '';
-    SEARCHKEYWORK.inputAdvanceKeyword = '';
-    SEARCHKEYWORK.selectedValue = '';
-    SEARCHKEYWORK.inputKeyword = $inputKeyword.val();
-    const newDataCombination = SEARCHKEYWORK.temporaryData.filter(data => data.address.search(SEARCHKEYWORK.inputKeyword) !== -1 || data.region.search(SEARCHKEYWORK.inputKeyword) !== -1);
+    pageStorage.inputKeyword = '';
+    pageStorage.inputAdvanceKeyword = '';
+    pageStorage.selectedValue = '';
+    pageStorage.inputKeyword = $inputKeyword.val();
+    const newDataCombination = pageStorage.temporaryData.filter(data => data.address.search(pageStorage.inputKeyword) !== -1 || data.region.search(pageStorage.inputKeyword) !== -1);
     pageStorage.machineData = newDataCombination;
     pageStorage.reloadRowPage();
   }
@@ -151,16 +144,16 @@ export default class Features {
   searchAdvancedData() {
     const { $inputAdvanceKeyword, pageStorage, selectedValue } = this;
     let newDataCombination = [];
-    SEARCHKEYWORK.inputKeyword = '';
-    SEARCHKEYWORK.inputAdvanceKeyword = $inputAdvanceKeyword.val();
-    SEARCHKEYWORK.selectedValue = selectedValue;
+    pageStorage.inputKeyword = '';
+    pageStorage.inputAdvanceKeyword = $inputAdvanceKeyword.val();
+    pageStorage.selectedValue = selectedValue;
 
     if (selectedValue !== undefined) {
-      SEARCHKEYWORK.selectedValue = selectedValue;
-      newDataCombination = SEARCHKEYWORK.temporaryData.filter(data => data.status.toString() === SEARCHKEYWORK.selectedValue && (data.address.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1 || data.region.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1));
+      pageStorage.selectedValue = selectedValue;
+      newDataCombination = pageStorage.temporaryData.filter(data => data.status.toString() === pageStorage.selectedValue && (data.address.search(pageStorage.inputAdvanceKeyword) !== -1 || data.region.search(pageStorage.inputAdvanceKeyword) !== -1));
     } else {
-      SEARCHKEYWORK.selectedValue = '';
-      newDataCombination = SEARCHKEYWORK.temporaryData.filter(data => data.address.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1 || data.region.search(SEARCHKEYWORK.inputAdvanceKeyword) !== -1);
+      pageStorage.selectedValue = '';
+      newDataCombination = pageStorage.temporaryData.filter(data => data.address.search(pageStorage.inputAdvanceKeyword) !== -1 || data.region.search(pageStorage.inputAdvanceKeyword) !== -1);
     }
     pageStorage.machineData = newDataCombination;
     pageStorage.reloadRowPage();
